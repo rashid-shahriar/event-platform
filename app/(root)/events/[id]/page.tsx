e8b21b8e9e8c5418162cff892159ce4560b13eab
rashid-shahriar <rashidslink@gmail.com>
2023-12-27 03:47:31 +0600

    working on create event details page

---
+++ app/(root)/events/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { getEventById } from "@/lib/actions/event.action";
+import { SearchParamProps } from "@/types";
+import console from "console";
+import Image from "next/image";
+import React from "react";
+
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+  const event = await getEventById(id);
+
+  return (
+    <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+        <Image
+          src={event.imageUrl}
+          alt="hero image"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
+
+        <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+          <div className="flex flex-col gap-6">
+            <h2 className="h2-bold">{event.title}</h2>
+
+            <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+              <div className="flex gap-3">
+                <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                  {event.isFree ? "FREE" : `${event.price}`}
+                </p>
+
+                <p className="p-medium-16 rounded-full bg-gray-500/10 px-4 py-2.5 text-gray-500">
+                  {event.category.name}
+                </p>
+              </div>
+
+              <p className="p-medium-18 ml-2 mt-2 sm:mt-0"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventDetails;
--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -9,6 +9,6 @@ const CategorySchema = new Schema({
   name: { type: String, required: true, unique: true },
 });
 
-const Category = models.Category || model("Catergory", CategorySchema);
+const Category = models.Category || model("Category", CategorySchema);
 
 export default Category;
